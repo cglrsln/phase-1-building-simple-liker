fbbccc3d960fb11237c74707b3f4e603c9f97a95
Caglar <caglarsulun@hotmail.com>
2021-12-09 00:51:54 -0500

    commit tree e9060d11bdf54fee03dbf6183a5f8da967d408dd

--- main.js
+++ main.js
@@ -2,7 +2,57 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
+
 // Your JavaScript code goes here!
+
+
+
+
+// hideModal function will hide the div(modal) when page is loaded
+// window.addEventListener('DOMContentLoaded', hideModal);
+
+// function hideModal() {
+//   const modal = document.querySelector("#modal");
+//   if (modal.style.display === "none") {
+//     mimicServerCall();
+//     modal.style.display = "block";
+//   } else {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hearts = Array.from(document.getElementsByClassName('like-glyph'));
+  hearts.forEach(heart => {
+    heart.addEventListener("click", function(event) {
+      mimicServerCall()
+      .then(function(response) {
+        if (heart.innerHTML === EMPTY_HEART) {
+          heart.className = "activated-heart";
+          heart.innerHTML = FULL_HEART;
+        } else {
+          heart.className = "like-glyph";
+          heart.innerHTML = EMPTY_HEART;
+        }
+      })
+      .catch(function(error) {
+        let modal = document.querySelector("#modal")
+        modal.className = "";
+        let modalMessage = document.querySelector("#modal-message");
+        modalMessage.innerHTML = error;
+        setTimeout(function() {
+          modal.className = "hidden" }, 3000);
+      })
+    })
+  })
+})
+
+
 
 
 
